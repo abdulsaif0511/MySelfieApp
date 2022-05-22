4db5f8cbce676a30fafb87c0dd2d6ff16c8702ac
abdulsaif0511 <82303862+abdulsaif0511@users.noreply.github.com>
2022-05-22 13:37:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,11 +13,14 @@ recognition.onresult = function(event) {
   console.log(event);
 
   var Content = event.results[0][0].transcript;
+  document.getElementById("textbox").innerHTML = Content ;
   console.log(Content);
 
-  document.getElementById("textbox").innerHTML = Content ;
-
-  speak();
+if(Content == "take my selfie")
+{
+console.log("taking selfie --- ")
+speak();
+}
 }
 
 
@@ -25,12 +28,18 @@ function speak(){
 
 var synth = window.speechSynthesis;
 
-speak_data = document.getElementById("textbox").ariaValueMax;
+speak_data = "taking your selfie in 5 seconds";
 
 var utterThis = new SpeechSynthesisUtterance(speak_data);
 
 synth.speak(utterThis);
  Webcam.attach(camera);
+
+setTimeout(function(){
+  take_snapshot();
+  save();
+},5000);
+
 }
 
 Webcam.set({
@@ -41,3 +50,21 @@ image_format : 'png' ,
 png_quality : 90
 });
 camera = document.getElementById("camera");
+  
+function take_snapshot(){
+
+  Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id = "selfie_image" src = "'+data_uri+'"/>';
+
+  });
+
+}
+
+function save()
+{
+
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src;
+  link.href = image;
+  link.click();
+}
